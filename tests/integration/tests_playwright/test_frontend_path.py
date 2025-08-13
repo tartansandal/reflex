2e54b446b7f6b13595b44e84b20d408fe31ecc19
Kahlil Hodgson <tartansandal@users.noreply.github.com>
2025-08-13 22:14:26 +1000

    Initial playwrite based test

---
+++ tests/integration/tests_playwright/test_frontend_path.py
@@ -0,0 +1,115 @@
+import os
+from collections.abc import Generator
+
+import pytest
+from playwright.sync_api import Page, expect
+
+from reflex.testing import AppHarness, AppHarnessProd
+
+
+def OnLoadRedirectApp():
+    import reflex as rx
+
+    @rx.page("/")
+    def index():
+        return rx.container(
+            rx.input(
+                value=rx.State.router.session.client_token,
+                read_only=True,
+                id="token",
+            ),
+            rx.vstack(
+                rx.heading("This is the index page!"),
+                rx.button("Go to Subpage!", on_click=rx.redirect("/subpage")),
+            ),
+        )
+
+    @rx.page("/subpage")
+    def subpage():
+        return rx.container(
+            rx.vstack(
+                rx.heading("This is the sub page!"),
+                rx.button("Go to index!", on_click=rx.redirect("/")),
+                rx.button("Bounce to index!", on_click=rx.redirect("/bouncer")),
+            )
+        )
+
+    @rx.page("/bouncer", on_load=rx.redirect("/"))
+    def bouncer():
+        return rx.container(
+            rx.vstack(
+                rx.heading("This is the bouncer page!"),
+                rx.text("You should not be here!"),
+                rx.button("Go to index!", on_click=rx.redirect("/")),
+            ),
+        )
+
+    app = rx.App()  # noqa: F841
+
+
+@pytest.fixture
+def onload_redirect_app(tmp_path_factory) -> Generator[AppHarness, None, None]:
+    with AppHarnessProd.create(
+        root=tmp_path_factory.mktemp("frontend_path_app"),
+        app_source=OnLoadRedirectApp,
+    ) as harness:
+        assert harness.app_instance is not None, "app is not running"
+        yield harness
+
+
+@pytest.fixture
+def onload_redirect_with_prefix_app(
+    tmp_path_factory,
+) -> Generator[AppHarness, None, None]:
+    # Set up environment for prefix serving
+    os.environ["REFLEX_FRONTEND_PATH"] = "/prefix"
+    with AppHarnessProd.create(
+        root=tmp_path_factory.mktemp("frontend_path_app"),
+        app_source=OnLoadRedirectApp,
+    ) as harness:
+        assert harness.app_instance is not None, "app is not running"
+        yield harness
+    os.environ.pop("REFLEX_FRONTEND_PATH", None)
+
+
+def test_onload_redirect(onload_redirect_app: AppHarness, page: Page):
+    assert onload_redirect_app.frontend_url is not None
+
+    base_url = onload_redirect_app.frontend_url.rstrip("/")
+    page.goto(base_url)
+    expect(page.get_by_text("This is the index page!")).to_be_visible()
+
+    # Go to /subpage
+    page.get_by_text("Go to Subpage!").click()
+    expect(page.get_by_text("This is the sub page!")).to_be_visible()
+
+    # Click "Bounce to index!" (should redirect to index via on_load)
+    page.get_by_text("Bounce to index!").click()
+    expect(page.get_by_text("This is the index page!")).to_be_visible()
+
+    # Optionally, reload /prefix/bouncer/ and check redirect
+    page.goto(f"{base_url}/bouncer/")
+    expect(page.get_by_text("This is the index page!")).to_be_visible()
+
+
+def test_onload_redirect_with_prefix(
+    onload_redirect_with_prefix_app: AppHarness, page: Page
+):
+    assert onload_redirect_with_prefix_app.frontend_url is not None
+
+    base_url = onload_redirect_with_prefix_app.frontend_url.rstrip("/")
+    base_url += "/prefix/"
+    page.goto(base_url)
+    expect(page.get_by_text("This is the index page!")).to_be_visible()
+
+    # Go to /subpage
+    page.get_by_text("Go to Subpage!").click()
+    expect(page.get_by_text("This is the sub page!")).to_be_visible()
+
+    # Click "Bounce to index!" (should redirect to index via on_load)
+    page.get_by_text("Bounce to index!").click()
+    expect(page.get_by_text("This is the index page!")).to_be_visible()
+
+    # # Optionally, reload /prefix/bouncer/ and check redirect
+    page.goto(f"{base_url}bouncer/")
+    expect(page.get_by_text("This is the index page!")).to_be_visible()
